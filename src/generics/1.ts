68a1c7d66d030dbe495ed6801bf3b309d6188c9f
Andriy Kiselyov <168927164+andrelux-ua@users.noreply.github.com>
2025-04-26 19:36:34 +0300

    fix-hw

--- src/generics/1.ts
+++ src/generics/1.ts
@@ -1,10 +1,8 @@
 import axios from "axios";
 
-export default async function fetchData<T extends object>(
-  url: string
-): Promise<T> {
+export default async function fetchData<T>(url: string): Promise<T> {
   try {
-    const response = await axios.get(url);
+    const response = await axios.get<T>(url);
     return response.data;
   } catch (error) {
     throw new Error(`Error fetching from ${url}: ${error}`);
